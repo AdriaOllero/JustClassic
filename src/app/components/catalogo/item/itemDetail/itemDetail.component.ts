df52281044242d08d0f781bf5defc44ef8cc6c3b
AdriaOllero <coronadoadrianollero@gmail.com>
2021-05-10 17:50:55 +0200

    ChangesChanges

--- src/app/components/catalogo/item/itemDetail/itemDetail.component.ts
+++ src/app/components/catalogo/item/itemDetail/itemDetail.component.ts
@@ -11,6 +11,7 @@ export class ItemDetailComponent implements OnInit {
 
   showOptions = true;
   showShare = false;
+  User = { name: 'Pepe', lastName:'Garcia',typeUser: 'Particular', img:'../../../../assets/images/perfil-pepe.jpg',location:'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.8920877215446!2d-78.61526208464393!3d-1.2346091990994936!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x91d381a86c8c8dff%3A0x9fc9691652c2ff32!2sLas%20Bahamas%2C%20Ambato%20180104%2C%20Ecuador!5e0!3m2!1ses!2ses!4v1619474341655!5m2!1ses!2ses' };
 
   Car = {
     bio:'Cillum laborum aliquip officia magna do sunt est fugiat id incididunt. Magna laboris officia veniam cillum id exercitation adipisicing consectetur. Id deserunt aute dolor sint esse non esse exercitation aute officia aliqua quis excepteur labore. Incididunt mollit velit labore minim velit ad cupidatat labore adipisicing veniam.\r\n',
--- src/app/components/main/topbar/topbar.component.js
+++
@@ -1,6 +0,0 @@
-$(function () {
-  $(document).onScroll(function () {
-    var $nav = $(".header");
-    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-  });
-});
